bbecf99e6521a034545d93a9c3b795ac3de9effb
Daniel L. Parton <Danny.Parton@gmail.com>
2015-09-03 16:31:11 -0400

    Update

--- mind2/core.py
+++ mind2/core.py
@@ -10,22 +10,24 @@ class Mind2MetaData(object):
     def get_note_filepaths(self):
         self.note_filepaths = [
             filepath for filepath in os.listdir(self.project_dir)
-            if len(filepath) > 3 and filepath[-3:] == '.md'
+            if (len(filepath) > 4 and filepath[-4:] == '.txt')
         ]
 
     def get_metadata(self):
         notes = {}
         tags = set()
         for note_filepath in self.note_filepaths:
-            note_name = note_filepath[: note_filepath.index('.md')]
+            note_name = note_filepath[: note_filepath.index('.txt')]
             notes.update({note_name: {}})
+            note_tags = []
+
             with open(note_filepath) as note_file:
                 for line in note_file:
-                    if len(line) >= 6 and line[0:6] == '@tags:':
-                        note_tags = line[6:].strip().split(',')
-                        break
-                tags.update(note_tags)
-                notes[note_name].update({'tags': note_tags})
+                    if len(line) >= 5 and line[0:5] == '@tag:':
+                        note_tags.append(line[5:].strip())
+
+            tags.update(note_tags)
+            notes[note_name].update({'tags': note_tags})
 
         self.notes = notes
         self.tags = tags
